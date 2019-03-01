7aaa5a54780a2a07b44e285b0dc7b7133d58d507
Alexbroking <alexbroking@gmail.com>
2019-03-01 13:46:49 +0100

    Player Movement

---
+++ Assets/scripts/LanesManager/LanesManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanesManager : MonoBehaviour
+{
+    public List<Vector3> lanes = new List<Vector3>();
+
+
+    void Awake()
+    {
+
+        // Adding Lanes Left / Middle / Right;
+        lanes.Add(new Vector3(-11.5f, -30, -69));
+        lanes.Add(new Vector3(-10, -30, -69));
+        lanes.Add(new Vector3(-8.5f, -30, -69));
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+  
+}
