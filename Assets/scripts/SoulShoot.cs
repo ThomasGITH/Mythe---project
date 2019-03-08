d2597f392aa6c85bd4249e4b54848787024f05a7
Sjors van der Kleij <24689@ma-web.nl>
2019-03-08 10:07:13 +0100

    soulshoot push
    soul shooting script

---
+++ Assets/scripts/SoulShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulShoot : MonoBehaviour
+{
+    [SerializeField]
+    private float BulletSpeed = 1;
+    private float timer;
+
+    void Update()
+    {
+        transform.Translate(0, BulletSpeed, 0);
+
+        timer += Time.deltaTime;
+        if (timer >= 2)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
