d61fda3da4cfcc47e42424d24ef59fddd7e33521
abdellbaz <abdellbaz@gmail.com>
2019-03-11 14:08:04 +0100

    test

--- Assets/scripts/Player/PlayerSideMovement.cs
+++ Assets/scripts/Player/PlayerSideMovement.cs
@@ -24,15 +24,16 @@ public class PlayerSideMovement : MonoBehaviour
     
     void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) { startTouchPosition = Input.GetTouch(0).position; }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) { startTouchPosition = Input.GetTouch(0).position;  }
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended || Input.anyKeyDown)
         {
             if (Input.touchCount > 0) { endTouchPosition = Input.GetTouch(0).position; }
 
+
             // Player Go Left
             if (playerLane != 0)
             {
-                if (Input.GetKeyDown(KeyCode.A)||endTouchPosition.x < startTouchPosition.x)
+                if (Input.GetKeyDown(KeyCode.A) || endTouchPosition.x < startTouchPosition.x)
                 {
                     playerLane -= 1;
                 }
@@ -41,13 +42,15 @@ public class PlayerSideMovement : MonoBehaviour
             // Player Go Right
             if (playerLane != (playerLanes.Count - 1))
             {
-                if (Input.GetKeyDown(KeyCode.D)|| endTouchPosition.x > startTouchPosition.x)
+                if (Input.GetKeyDown(KeyCode.D) || endTouchPosition.x > startTouchPosition.x)
                 {
                     playerLane += 1;
                 }
             }
         }
+        
         // Set Player Position (While Moving)
         player.transform.position = Vector3.MoveTowards(player.transform.position, playerLanes[playerLane], playerStep * Time.deltaTime);
     }
+    
 }
--- Assets/scripts/Player/SwipeMovement.cs
+++ Assets/scripts/Player/SwipeMovement.cs
@@ -6,24 +6,31 @@ public class SwipeMovement : MonoBehaviour
 {
 
     private Vector3 startTouchPosition, endTouchPosition, LeftPosition, RightPosition;
+    private bool CanMove;
+    private float Distance =2.5f;
     private void Start()
     {
-        LeftPosition = new Vector3(transform.position.x - 2.5f, transform.position.y, transform.position.z);
-        RightPosition = new Vector3(transform.position.x + 2.5f, transform.position.y, transform.position.z);
+        
+        LeftPosition = new Vector3(transform.position.x - Distance, transform.position.y, transform.position.z);
+        RightPosition = new Vector3(transform.position.x + Distance, transform.position.y, transform.position.z);
+
 
     }
 
     private void Update()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) { startTouchPosition = Input.GetTouch(0).position; }
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended|| Input.anyKeyDown)
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) { CanMove = true; }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved && CanMove || Input.anyKeyDown)
         {
            if (Input.touchCount > 0) { endTouchPosition = Input.GetTouch(0).position; }
     
         if (Input.GetKeyDown(KeyCode.A)|| (endTouchPosition.x < startTouchPosition.x))
         {
             if (LeftPosition != transform.position) {
-            transform.position = new Vector3(transform.position.x - 2.5f, transform.position.y,transform.position.z);
+            transform.position = new Vector3(transform.position.x - Distance, transform.position.y,transform.position.z);
+                    CanMove = false;
             }
 
         }
@@ -31,8 +38,9 @@ public class SwipeMovement : MonoBehaviour
         {
             if (RightPosition != transform.position)
             {
-                transform.position = new Vector3(transform.position.x + 2.5f, transform.position.y, transform.position.z);
-            }
+                transform.position = new Vector3(transform.position.x + Distance, transform.position.y, transform.position.z);
+                    CanMove = false;
+                }
 
         }
         }
