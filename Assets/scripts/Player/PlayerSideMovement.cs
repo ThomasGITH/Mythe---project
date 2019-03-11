186e00806f4329b9f13f908ced0638ae327348b7
abdellbaz <abdellbaz@gmail.com>
2019-03-11 14:28:12 +0100

    Revert "Merge branch 'mobile'"
    This reverts commit 335d1fbe4b393aad2be8c227344235fa15c45da9, reversing
    changes made to 120e43c9dec818d6d55012d5128692c547d1d4a7.

---
+++ Assets/scripts/Player/PlayerSideMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSideMovement : MonoBehaviour
+{
+    private LanesManager Lanes;
+    private GameObject player;
+    public int playerLane = 1;
+    private float playerStep = 10;
+
+    List<Vector3> playerLanes = new List<Vector3>();
+
+    void Start()
+    {
+        //Get player / Get Positions & Set position 
+        player = GameObject.Find("Player");
+        Lanes = player.GetComponent<LanesManager>();
+        playerLanes = LanesManager.lanes;
+        player.transform.position = playerLanes[1];
+    }
+
+    
+    void Update()
+    {
+        // Player Go Left
+        if (playerLane == 1 || playerLane == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                playerLane -= 1;
+            }
+        }
+
+        // Player Go Right
+        if (playerLane == 0 || playerLane == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                playerLane += 1;
+            }
+        }
+
+        // Set Player Position (While Moving)
+        player.transform.position = Vector3.MoveTowards(player.transform.position, playerLanes[playerLane], playerStep * Time.deltaTime);
+    }
+}
