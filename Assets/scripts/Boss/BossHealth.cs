28f7fc55f1dfceac5667f4a1f564a91b643416cd
Alexbroking <alexbroking@gmail.com>
2019-03-14 09:14:43 +0100

    Delete horse no rig.fbx

---
+++ Assets/scripts/Boss/BossHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Brandon Ruigrok
+public class BossHealth : MonoBehaviour
+{
+    int Hp = 400;
+
+   // DamageOutput dmg;
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+    //    dmg = GameObject.Find("Scyth").GetComponent<DamageOutput>();
+        if (col.gameObject.tag == "Shot Soul")
+        {
+    //        Hp = (Hp - dmg.Damage);
+        }
+    }
+}
