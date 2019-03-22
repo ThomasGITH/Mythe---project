c9fe597419759332c9d1e2b6f6097f9d7a7d2146
abdellbaz <abdellbaz@gmail.com>
2019-03-22 09:44:34 +0100

    portal

---
+++ Assets/scripts/Boss/BossBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBehavior : MonoBehaviour
+{
+
+    private Vector3 bossPosition = new Vector3(-10, -30, -30);
+    private Vector3 spawningBossPosition = new Vector3(-10, -30, 255);
+    [SerializeField]
+    private bool bossIsActive = false;
+    private bool bossIsInPosition = false;
+    private GameObject spawningBossObject;
+    [SerializeField]
+    private GameObject boss;
+    private float bossFlyingSpeed =  35;
+    private float bossWaitingSpawnTimeSeconds = 5;
+    private SoulSpawner soulSpawner;
+
+    void Start()
+    {
+        spawningBossObject = Resources.Load("Portal") as GameObject;
+        soulSpawner = GameObject.FindWithTag("SoulSpawner").GetComponent<SoulSpawner>();
+        soulSpawner.enabled = false;
+    }
+
+    
+    void Update()
+    {
+        // Check if boss is alive
+        if (bossIsActive == false)
+        {
+            StartCoroutine(BossSpawning());
+            bossIsActive = true;
+        }
+
+        if (boss != null)
+        {
+
+            // Check if souls can be spawned
+            if (boss.transform.position == bossPosition)
+            {
+                soulSpawner.enabled = true;
+            }
+
+            // Make boss move to position
+            if (boss.transform.position != spawningBossPosition)
+            {
+                boss.transform.position = Vector3.MoveTowards(boss.transform.position, bossPosition, bossFlyingSpeed * Time.deltaTime);
+            }
+        }
+    }
+
+
+    //Wait for boss spawning and spawns it
+    IEnumerator BossSpawning()
+    {
+        bossIsActive = true;
+        yield return new WaitForSeconds(bossWaitingSpawnTimeSeconds);
+        Instantiate(spawningBossObject, spawningBossPosition, Quaternion.identity);
+        boss = GameObject.FindWithTag("Portal");
+    }
+}
---
+++ Assets/scripts/Pause/PauseButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+//Brandon Ruigrok
+public class PauseButtons : MonoBehaviour
+{
+    string Menu = "Menu";
+
+    public void ResumeButton()
+    {
+        gameObject.GetComponentInParent<Pause>().Resumed();
+    }
+
+    public void PauseButton()
+    {
+        gameObject.GetComponentInParent<Pause>().Paused();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(Menu);
+    }
+}
