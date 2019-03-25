b4b5cf3d50e65df7681b1d3e122834c33d028629
Alexbroking <alexbroking@gmail.com>
2019-03-25 09:47:15 +0100

    Bosshealth & Death

--- Assets/scripts/Boss/BossBehavior.cs
+++ Assets/scripts/Boss/BossBehavior.cs
@@ -7,15 +7,17 @@ public class BossBehavior : MonoBehaviour
 
     private Vector3 bossPosition = new Vector3(-10, -30, -30);
     private Vector3 spawningBossPosition = new Vector3(-10, -30, 255);
-    [SerializeField]
+    private Vector3 bossDeadPosition = new Vector3(-10, -30, -75);
     private bool bossIsActive = false;
     private bool bossIsInPosition = false;
+    private bool canSpawnOtherBoss = false;
     private GameObject spawningBossObject;
-    [SerializeField]
     private GameObject boss;
-    private float bossFlyingSpeed =  35;
+    private float bossFlyingSpeed = 35;
     private float bossWaitingSpawnTimeSeconds = 5;
     private SoulSpawner soulSpawner;
+    private int bossFirstTime = 1;
+    private bool bossJustDied = false;
 
     void Start()
     {
@@ -24,43 +26,75 @@ public class BossBehavior : MonoBehaviour
         soulSpawner.enabled = false;
     }
 
-    
+
     void Update()
     {
         // Check if boss is alive
         if (bossIsActive == false)
         {
             StartCoroutine(BossSpawning());
+            canSpawnOtherBoss = false;
             bossIsActive = true;
         }
 
         if (boss != null)
         {
-
             // Check if souls can be spawned
-            if (boss.transform.position == bossPosition)
+            if (boss.transform.position == bossPosition && bossJustDied == false)
             {
                 soulSpawner.enabled = true;
             }
 
             // Make boss move to position
-            if (boss.transform.position != spawningBossPosition)
+            if (boss.transform.position != spawningBossPosition && bossJustDied == false)
             {
                 boss.transform.position = Vector3.MoveTowards(boss.transform.position, bossPosition, bossFlyingSpeed * Time.deltaTime);
-                print(boss.transform.position);
+            }
+
+            if (bossJustDied == true)
+            {
+                boss.transform.position = Vector3.MoveTowards(boss.transform.position, bossDeadPosition, bossFlyingSpeed * Time.deltaTime);
+            }
+
+            if (boss.transform.position == bossDeadPosition)
+            {
+                bossJustDied = false;
+                Destroy(boss);
             }
         }
-       
+
+        if (boss == null)
+        {
+            if (canSpawnOtherBoss == true)
+            {
+                soulSpawner.enabled = false;
+                bossIsActive = false;
+            }
+        }
+
+
+    }
+
+    public void BossDied()
+    {
+        bossJustDied = true;
     }
 
 
     //Wait for boss spawning and spawns it
     IEnumerator BossSpawning()
     {
-        bossIsActive = true;
+        if (bossFirstTime != 1)
+        {
+            yield return new WaitForSeconds(bossWaitingSpawnTimeSeconds);
+        }
+        else
+        {
+            bossFirstTime += 1;
+        }
         yield return new WaitForSeconds(bossWaitingSpawnTimeSeconds);
         Instantiate(spawningBossObject, spawningBossPosition, Quaternion.identity);
         boss = GameObject.FindWithTag("Portal");
-        boss.transform.position = new Vector3(boss.transform.position.x, boss.transform.position.y, boss.transform.position.z - 10);
+        canSpawnOtherBoss = true;
     }
 }
--- Assets/scripts/Boss/BossHealth.cs
+++ Assets/scripts/Boss/BossHealth.cs
@@ -9,7 +9,14 @@ public class BossHealth : MonoBehaviour
     [SerializeField]
     int Hp = 400;
 
+    public BossBehavior bossDeath;
     public Action<int> BossTakingDamage;
+    private bool canDie = true;
+
+    private void Start()
+    {
+        bossDeath = GameObject.Find("Player").GetComponent<BossBehavior>();
+    }
 
     public void TakeDamage(int damage)
     {
@@ -18,6 +25,18 @@ public class BossHealth : MonoBehaviour
         if (BossTakingDamage != null)
         {
             BossTakingDamage(Hp);
+        }
+    }
+
+    private void Update()
+    {
+        if (canDie == true)
+        {
+            if (Hp <= 0)
+            {
+                bossDeath.BossDied();
+                canDie = false;
+            }
         }
     }
 }
