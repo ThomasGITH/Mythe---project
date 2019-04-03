81ea92b32fb16106018d2ec7f4f1f96b103fa2f2
abdellbaz <abdellbaz@gmail.com>
2019-04-03 21:35:07 +0200

    brug 2 in de game

--- Assets/scripts/single_use/Grid.cs
+++ Assets/scripts/single_use/Grid.cs
@@ -7,14 +7,14 @@ public class Grid : MonoBehaviour
     public GameObject cube;
     public GameObject[] objs;
 
-
+    public float num = 5.035f;
 
     void Start()
     {
-        for (int i = 0; i < 15; i++)
+        for (int i = 0; i < 25; i++)
         {
-            objs[i] = Instantiate(cube, transform);//5.035 eerste brug
-            objs[i].transform.position += new Vector3(0f, 0f,i*5.035f);
+            objs[i] = Instantiate(cube, transform);//5.035 eerste brug 2.944 twee de bruh
+            objs[i].transform.position += new Vector3(0f, 0f,i* num);
         }
 
     }
